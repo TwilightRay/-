3134841c70ce6f5e91a9c8761b97ee747f524273
TwilightRay <42698640+TwilightRay@users.noreply.github.com>
2018-09-24 00:22:24 +0600

    Add files via upload

--- Практика2.py
+++ Практика2.py
@@ -6,3 +6,22 @@ def add_five(x):
 def add_seven(u):
     return u+7
 print(apply_twice(add_five,10,add_seven,500))
+print("\n\n")
+
+def pure_function(x,y):
+    temp=x+2*y
+    return temp/(2*x+y)
+print(pure_function(50,20))
+print("\n\n")
+some_list=[]
+def impure(arg):
+    some_list.append(arg)
+    print(some_list)
+a=5
+while a>0:
+    arg=a
+    a-=1
+    impure(arg)
+impure(some_list)
+print("\n\n")
+
