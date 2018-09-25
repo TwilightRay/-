ffd693c26df4205e251185108145ddcf769973ed
TwilightRay <42698640+TwilightRay@users.noreply.github.com>
2018-09-25 23:58:44 +0600

    Add files via upload

--- Практика2.py
+++ Практика2.py
@@ -65,3 +65,18 @@ def countdown():
         i -= 1
 for i in countdown():
     print(i)
+
+print("\n\n")
+
+def numbers(x):
+    for i in range(x):
+        if i%2==0:
+            yield i
+print(list(numbers(11)))#преобразование генерации в список
+
+def make_word():
+    word=''
+    for ch in 'spam':
+        word+=ch
+        yield word
+print(list(make_word()))
