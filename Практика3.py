c0d803348f77f86a4aa3caebdce9a911a14559af
TwilightRay <42698640+TwilightRay@users.noreply.github.com>
2018-10-01 20:57:23 +0600

    Add files via upload

--- Практика3.py
+++ Практика3.py
@@ -1,17 +1,42 @@
-class Cat:
-    def __init__(self,color,legs):
-        self.color=color
-        self.legs=legs
-felix=Cat("Ginger",4)
-print(felix.color)
-rover=Cat("dog-colored",4)
-stumpy=Cat("brown",3)
+class Student:
+    def __init__(self,name):
+        self.name=name
+    def sayHi(self):
+        print("Hi from "+ self.name)
+s1=Student("Amy")
+s1.sayHi()
+test=Student("Bob")
 
-class Dog:
-    legs=4
+class Rectangle:
+    def __init__(self,width,height):
+        self.width=width
+        self.height=height
+rect=Rectangle(7,8)
+print(rect)
+
+
+class Animal:  #суперкласс
     def __init__(self,name,color):
         self.name=name
         self.color=color
+
+class Cat(Animal): #наследуемый класс
+    #def __init__(self,color,legs): а низя
+    #   self.color=color
+    #  self.legs=legs
+    def purr(self):
+        print("Purr...")
+
+#felix=Cat("Ginger",4)
+#print(felix.color)
+#rover=Cat("dog-colored",4)
+#stumpy=Cat("brown",3)
+
+class Dog(Animal):
+    legs=4
+    #def __init__(self,name,color):
+     #   self.name=name
+      #  self.color=color
     def bark(self):
         print("Woof!")
 fido=Dog("Fido","brown")
@@ -20,10 +45,4 @@ fido.bark()
 print(fido.legs)
 print(Dog.legs)
 
-class Student:
-    def __init__(self,name):
-        self.name=name
-    def sayHi(self):
-        print("Hi from "+ self.name)
-s1=Student("Amy")
-s1.sayHi()
+
