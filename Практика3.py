626bdc08ba8de8ef698d2a7afc6cfae963be7c3c
TwilightRay <42698640+TwilightRay@users.noreply.github.com>
2018-10-04 23:59:53 +0600

    Add files via upload

--- Практика3.py
+++ Практика3.py
@@ -89,3 +89,10 @@ second=Vector2D(3,9)
 result=first+second
 print(result.x)
 print(result.y)
+
+class SpecialString:
+    def __init__(self,cont):
+        self.cont=cont
+    def __truediv__(self, other):
+        line="="*len(other.cont)
+        return "\n".join([self.cont,line,other.cont])
