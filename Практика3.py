3c1d07eec06a511a43ef393c67e7604c9f84c440
TwilightRay <42698640+TwilightRay@users.noreply.github.com>
2018-10-05 00:23:55 +0600

    Add files via upload

--- Практика3.py
+++ Практика3.py
@@ -90,9 +90,40 @@ result=first+second
 print(result.x)
 print(result.y)
 
-class SpecialString:
-    def __init__(self,cont):
-        self.cont=cont
+class Def:
+    def __init__(self, cont):
+        self.cont = cont
+
+class SpecialString(Def):
     def __truediv__(self, other):
         line="="*len(other.cont)
         return "\n".join([self.cont,line,other.cont])
+
+spam=SpecialString("spam")
+hello=SpecialString("Hello world")
+print(spam/hello)
+
+class SpecialString1(Def):
+    def __gt__(self,other):
+        for index in range(len(other.cont)+1):
+            result=other.cont[:index]+">"+self.cont
+            result+=">"+other.cont[:index]
+            print(result)
+
+spam = SpecialString1("spam")
+eggs = SpecialString1("eggs")
+spam > eggs
+
+import random
+class VagueList(Def):
+    def __getitem__(self, index):
+        return self.cont[index+random.randint(-1,1)]
+
+    def __len__(self):
+        return random.randint(0,len(self.cont)*2)
+
+vague_list=VagueList(["A","B","C","D","E"])
+print(len(vague_list))
+print(len(vague_list))
+print(vague_list[2])
+print(vague_list[2])
