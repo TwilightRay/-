06996de006ff9e5447bfbf4cbf3b40d9b41202ee
TwilightRay <42698640+TwilightRay@users.noreply.github.com>
2018-10-06 00:02:09 +0600

    Add files via upload

---
+++ Практика 4.py
@@ -0,0 +1,66 @@
+class Vector2D:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    def __add__(self, other):
+        return Vector2D(self.x+other.x, self.y+other.y)
+first=Vector2D(5,7)
+second=Vector2D(3,9)
+result=first+second
+print(result.x)
+print(result.y)
+
+class Def:
+    def __init__(self, cont):
+        self.cont = cont
+
+class SpecialString(Def):
+    def __truediv__(self, other):
+        line="="*len(other.cont)
+        return "\n".join([self.cont,line,other.cont])
+
+spam=SpecialString("spam")
+hello=SpecialString("Hello world")
+print(spam/hello)
+
+class SpecialString1(Def):
+    def __gt__(self,other):
+        for index in range(len(other.cont)+1):
+            result=other.cont[:index]+">"+self.cont
+            result+=">"+other.cont[:index]
+            print(result)
+
+spam = SpecialString1("spam")
+eggs = SpecialString1("eggs")
+spam > eggs
+
+import random
+class VagueList(Def):
+    def __getitem__(self, index):
+        return self.cont[index+random.randint(-1,1)]
+
+    def __len__(self):
+        return random.randint(0,len(self.cont)*2)
+
+vague_list=VagueList(["A","B","C","D","E"])
+print(len(vague_list))
+print(len(vague_list))
+print(vague_list[2])
+print(vague_list[2])
+
+class Queue:
+    def __init__(self, contents):
+        self._hiddenlist = list(contents)
+    def push (self,value):
+        self._hiddenlist.insert(0,value)
+    def pop(self):
+        return self._hiddenlist.pop(-1)
+    def __repr__(self):
+        return "Queue({})".format(self._hiddenlist)
+queue = Queue([1,2,3])
+print(queue)
+queue.push(0)
+print(queue)
+queue.pop()
+print(queue)
+print(queue._hiddenlist)
