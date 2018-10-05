10bdafa2c7d01c723c6ecd6eff796dcfa35401c4
TwilightRay <42698640+TwilightRay@users.noreply.github.com>
2018-10-05 23:59:29 +0600

    Add files via upload

--- Практика3.py
+++ Практика3.py
@@ -127,3 +127,19 @@ print(len(vague_list))
 print(len(vague_list))
 print(vague_list[2])
 print(vague_list[2])
+
+class Queue:
+    def __init__(self, contents):
+        self._hiddenlist = list(contents)
+    def push (self,value):
+        self._hiddenlist.insert(0,value)
+    def pop(self):
+        return self._hiddenlist.pop(-1)
+    def __repr__(self):
+        return "Queue({})".format(self._hiddenlist)
+queue = Queue([1,2,3])
+print(queue)
+queue.push(0)
+print(queue)
+queue.pop()
+print(queue)
