fa343bd858a13a3b46d05d646979ba1eb7ce5e0b
TwilightRay <42698640+TwilightRay@users.noreply.github.com>
2018-10-08 21:48:42 +0600

    Add files via upload

--- Практика 4.py
+++ Практика 4.py
@@ -64,3 +64,12 @@ print(queue)
 queue.pop()
 print(queue)
 print(queue._hiddenlist)
+
+class Spam:
+    __egg=7
+    def print_egg(self):
+        print(self.__egg)
+s=Spam()
+s.print_egg()
+print(s._Spam__egg)
+print(s.__egg)
