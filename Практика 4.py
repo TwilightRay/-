47ce9fa119ca95bbaa98dfbc619c0a8c3ffb714e
TwilightRay <42698640+TwilightRay@users.noreply.github.com>
2018-10-12 20:28:21 +0600

    Add files via upload

--- Практика 4.py
+++ Практика 4.py
@@ -107,3 +107,23 @@ print(pizza.pineapple_allowed)
 pizza.pineapple_allowed=True
 print(pizza.pineapple_allowed)
 
+def get_input():
+    command=input(": ").split()
+    verb_word = command[0]
+    if verb_word in verb_dict:
+        verb=verb_dict[verb_word]
+    else:
+        print("Unknown verb {}".format(verb_word))
+        return
+    if len(command)>=2:
+        noun_word=command[1]
+        print(verb(noun_word))
+    else:
+        print(verb())
+def say(noun):
+    return 'You said "{}"'.format(noun)
+verb_dict={
+    "say": say,
+}
+while True:
+    get_input()
